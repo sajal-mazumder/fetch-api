d6b786ad1c44bcce79c2c4dde43cf8a38f488cbf
sajal-mazumder <sajalmazumder47@gmail.com>
2025-05-12 00:01:19 +0600

    some update

--- app.js
+++ app.js
@@ -60,47 +60,81 @@
 
 // %%% method three create by div
 
-const container = document.getElementById("container")
-container.style.display = "flex"
-container.style.flexWrap = "wrap"
-container.style.justifyContent = "space-around"
-container.style.alignItems = "center"
+// const container = document.getElementById("container")
+// container.style.display = "flex"
+// container.style.flexWrap = "wrap"
+// container.style.justifyContent = "space-around"
+// container.style.alignItems = "center"
 // container.style.padding = "10px"
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-.then(res => res.json())
-.then(allData => {
-    allData.forEach(ele => {
-        const div = document.createElement("div")
-        container.appendChild(div)
-        div.style.width = "250px"
-        div.style.height = "200px"
-        div.style.backgroundColor = "#696969"
-        div.style.marginBottom = "10px"
+// fetch("https://jsonplaceholder.typicode.com/posts")
+// .then(res => res.json())
+// .then(allData => {
+//     allData.forEach(ele => {
+//         const div = document.createElement("div")
+//         container.appendChild(div)
+//         div.style.width = "250px"
+//         div.style.height = "200px"
+//         div.style.backgroundColor = "#696969"
+//         div.style.marginBottom = "10px"
 
-        const divh2 = document.createElement("h2")
-        divh2.style.color = "#fff"
-        divh2.style.fontFamily = "arial"
-        divh2.style.padding = "5px"
-        divh2.innerHTML = "ID:" + " " + ele.id
-        div.appendChild(divh2)
+//         const divh2 = document.createElement("h2")
+//         divh2.style.color = "#fff"
+//         divh2.style.fontFamily = "arial"
+//         divh2.style.padding = "5px"
+//         divh2.innerHTML = "ID:" + " " + ele.id
+//         div.appendChild(divh2)
 
-        const divh4 = document.createElement("h4")
-        divh4.style.color = "#fff"
-        divh4.style.fontFamily = "arial"
-        divh4.style.padding = "5px"
-        divh4.innerHTML = "TITLE:" + " " + ele.title
-        div.appendChild(divh4);
+//         const divh4 = document.createElement("h4")
+//         divh4.style.color = "#fff"
+//         divh4.style.fontFamily = "arial"
+//         divh4.style.padding = "5px"
+//         divh4.innerHTML = "TITLE:" + " " + ele.title
+//         div.appendChild(divh4);
 
-        const divp = document.createElement("p")
-        divp.style.color = "#fff"
-        divp.style.fontFamily = "arial"
-        divp.style.padding = "5px"
-        // divp.style.overflow = "hidden"
-        divp.innerHTML = "BODY:" + " " + ele.body
-        div.appendChild(divp)
+//         const divp = document.createElement("p")
+//         divp.style.color = "#fff"
+//         divp.style.fontFamily = "arial"
+//         divp.style.padding = "5px"
+//         // divp.style.overflow = "hidden"
+//         divp.innerHTML = "BODY:" + " " + ele.body
+//         div.appendChild(divp)
         
-    });
-})
+//     });
+// })
 
 //%%% end of method three
+
+// &&&& method four
+
+const container = document.getElementById("container")
+// container.style.display = "flex"
+// container.style.flexWrap = "wrap"
+// container.style.justifyContent = "space-around"
+// container.style.alignItems = "center"
+// container.style.padding = "10px"
+
+const url = "https://jsonplaceholder.typicode.com/posts"
+
+fetch(url)
+.then(res => res.json())
+.then(dt => {
+    dt.forEach(element => {
+        const div = document.createElement("div")
+
+        // div.style.marginBottom = "10px"
+        // div.style.backgroundColor = "#696969"
+        // div.style.padding = "10px"
+        // div.style.color = "#fff"
+        // div.style.width = "200px"
+        // div.style.height = "180px"
+
+        div.innerHTML = `
+        ID: ${element.id}<br>
+        TITLE: ${element.title}<br>
+        BODY: ${element.body}<br>
+        `
+
+        container.appendChild(div)
+    });
+})
